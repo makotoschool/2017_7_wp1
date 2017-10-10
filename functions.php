9f16719a5cf412fda0672fc9f9e625ba4ad01c45
Makoto Imura <jamzip@gmail.com>
2017-10-10 15:07:10 +0900

    test

---
+++ functions.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+function child_theme_scripts( ) {
+     wp_enqueue_style( 'parent-theme-css', get_template_directory_uri( ) . '/style.css' );
+    }
+
+    add_action( 'wp_enqueue_scripts', 'child_theme_scripts' );
+
+
